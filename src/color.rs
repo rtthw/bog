4fa4564437f2e3abf99a8582e1b6381f20f10cd1
rtthw <172864013+rtthw@users.noreply.github.com>
2024-12-15 14:34:47 -0500

    Implement more color utilities

--- src/color.rs
+++ src/color.rs
@@ -18,23 +18,59 @@ impl From<u32> for Color {
 }
 
 impl Color {
+    /// Create a fully-opaque color from its red, green, and blue values.
+    #[inline]
+    pub const fn from_rgb(r: u8, g: u8, b: u8) -> Self {
+        Self([r, g, b, 255])
+    }
+
+    /// Create a color from its red, green, blue, and alpha channels.
+    #[inline]
+    pub const fn from_rgba(r: u8, g: u8, b: u8, a: u8) -> Self {
+        Self([r, g, b, a])
+    }
+
+    /// Create an opaque color with red, green, and blue channels that all have the same value.
+    #[inline]
+    pub const fn from_gray(v: u8) -> Self {
+        Self([v, v, v, 255])
+    }
+}
+
+impl Color {
+    /// Red value.
     #[inline]
     pub const fn r(&self) -> u8 {
         self.0[0]
     }
 
+    /// Green value.
     #[inline]
     pub const fn g(&self) -> u8 {
         self.0[1]
     }
 
+    /// Blue value.
     #[inline]
     pub const fn b(&self) -> u8 {
         self.0[2]
     }
 
+    /// Alpha value.
     #[inline]
     pub const fn a(&self) -> u8 {
         self.0[3]
+    }
+
+    /// Red, green, blue values as a tuple.
+    #[inline]
+    pub const fn rgb_tuple(&self) -> (u8, u8, u8) {
+        (self.0[0], self.0[1], self.0[2])
+    }
+
+    /// Red, green, blue, and alpha values as a tuple.
+    #[inline]
+    pub const fn rgba_tuple(&self) -> (u8, u8, u8, u8) {
+        (self.0[0], self.0[1], self.0[2], self.0[3])
     }
 }
