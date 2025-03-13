c9d63ac70823153144136776e7145af3e74fbf56
rtthw <172864013+rtthw@users.noreply.github.com>
2025-03-13 07:00:57 -0400

    Add some basic type definitions to types library

--- bog_types/src/lib.rs
+++ bog_types/src/lib.rs
@@ -1,3 +1,44 @@
 //! Bog Types
 //!
 //! The set of type definitions used in the bog environment and runtime.
+
+
+
+pub struct Message {
+    pub code: [u8; 4],
+    pub sender: u32,
+    pub data: MessageData,
+}
+
+pub enum MessageData {
+    Info, // TODO: arrayvec::ArrayString
+    Warning, // TODO: arrayvec::ArrayString
+    Error, // TODO: arrayvec::ArrayString
+
+    DeviceInput {
+        device_id: u32,
+    },
+}
+
+/// A request is a [message](crate::Message) that expects a response.
+///
+/// Because of this need for a synchronous response from the environment, they are sent across a
+/// different channel from the normal messages.
+pub struct Request {
+    pub code: [u8; 4],
+    pub sender: u32,
+    pub data: RequestData,
+}
+
+pub enum RequestData {
+    CreateWindow, // TODO: arrayvec::ArrayString
+}
+
+pub struct Reply {
+    pub success: bool,
+    pub data: ReplyData,
+}
+
+pub enum ReplyData {
+    WindowCreated(u32),
+}
