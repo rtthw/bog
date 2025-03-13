4cc295c2895c092945bd5816e525f331bf37d179
rtthw <172864013+rtthw@users.noreply.github.com>
2025-03-13 07:42:56 -0400

    Refine testing implementation

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,20 +1,46 @@
 
 
 
+use std::time::Instant;
+
 use bog_types::*;
 use env::*;
 
 
+/*
+
+NOTES:
+    - As of 3/13/25, on my Linux workstation:
+        DEBUG: 1000 requests => 1.5 ms
+        RELEASE: 1000 requests => 0.5 ms
+
+*/
+
+const REQ_COUNT: usize = 1000;
+
+
 
 fn main() -> Result<()> {
-    let mut conn = connect()?;
-    let reply = conn.request(Request {
-        code: [0, 0, 0, 0],
-        sender: conn.id(),
-        data: RequestData::CreateWindow,
-    })?;
+    let start = Instant::now();
 
-    println!("Got reply: {:?}", reply);
+    let mut conn = connect()?;
+
+    println!("\n\tPerforming {} requests...", REQ_COUNT);
+
+    for _ in 0..REQ_COUNT {
+        let reply = conn.request(Request {
+            code: [0, 0, 0, 0],
+            sender: conn.id(),
+            data: RequestData::CreateWindow,
+        })?;
+        if !reply.success {
+            println!("ERROR");
+        }
+    }
+
+    let time_taken = Instant::now().duration_since(start);
+
+    println!("\n\tTook {}ns, or {} ms", time_taken.as_nanos(), time_taken.as_millis());
 
     Ok(())
 }
