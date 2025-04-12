33640842276b5650275d4d89ba040ce611be7bb6
rtthw <172864013+rtthw@users.noreply.github.com>
2025-04-12 10:16:50 -0400

    Finish most of the painter's core functionality

--- src/graphics/painter.rs
+++ src/graphics/painter.rs
@@ -2,7 +2,7 @@
 
 
 
-use crate::math::Vec2;
+use std::ops::Range;
 
 use super::{RenderPass, Shader, ShaderDescriptor, Vertex, WindowGraphics};
 
@@ -11,9 +11,8 @@ use super::{RenderPass, Shader, ShaderDescriptor, Vertex, WindowGraphics};
 // TODO: It might be better to create a custom `PaintVertex` type.
 pub struct Painter {
     shader: Shader,
-    vertex_buffer: wgpu::Buffer,
-    index_buffer: wgpu::Buffer,
-    num_indices: u32,
+    vertex_buffer: PaintBuffer,
+    index_buffer: PaintBuffer,
 }
 
 impl Painter {
@@ -35,49 +34,160 @@ impl Painter {
             })],
             ..Default::default()
         });
-        let vertex_buffer = graphics.device().create_buffer(&wgpu::BufferDescriptor {
+
+        const VBUF_CAP: wgpu::BufferAddress = (std::mem::size_of::<Vertex>() * 1024) as _;
+        const IBUF_CAP: wgpu::BufferAddress = (std::mem::size_of::<u32>() * 1024 * 3) as _;
+
+        let vertex_buffer_inner = graphics.device().create_buffer(&wgpu::BufferDescriptor {
             label: Some("Painter Vertex Buffer"),
-            size: 64,
-            usage: wgpu::BufferUsages::VERTEX,
+            size: VBUF_CAP,
+            usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
             mapped_at_creation: false,
         });
-        let index_buffer = graphics.device().create_buffer(&wgpu::BufferDescriptor {
+        let index_buffer_inner = graphics.device().create_buffer(&wgpu::BufferDescriptor {
             label: Some("Painter Index Buffer"),
-            size: 64,
-            usage: wgpu::BufferUsages::INDEX,
+            size: IBUF_CAP,
+            usage: wgpu::BufferUsages::INDEX | wgpu::BufferUsages::COPY_DST,
             mapped_at_creation: false,
         });
 
         Self {
             shader,
-            vertex_buffer,
-            index_buffer,
-            num_indices: 0,
+            vertex_buffer: PaintBuffer {
+                inner: vertex_buffer_inner,
+                slices: Vec::with_capacity(64),
+                capacity: VBUF_CAP,
+            },
+            index_buffer: PaintBuffer {
+                inner: index_buffer_inner,
+                slices: Vec::with_capacity(64),
+                capacity: IBUF_CAP,
+            },
         }
     }
 
-    pub fn render(&self, mut pass: RenderPass, paints: &[Paint]) {
-        pass.use_shader(&self.shader);
+    pub fn prepare(&mut self, graphics: &WindowGraphics, paints: &[PaintMesh]) {
         let (num_vertices, num_indices) = paints.iter().fold((0, 0), |acc, paint| {
-            match paint {
-                Paint::Quad { .. } => {
-                    (acc.0 + 4, acc.1 + 6)
-                }
-            }
+            (acc.0 + paint.vertices.len(), acc.1 + paint.indices.len())
         });
 
-        for paint in paints {
+        if num_indices > 0 {
+            self.index_buffer.slices.clear();
 
+            let required_buffer_size = (std::mem::size_of::<u32>() * num_indices) as u64;
+            if self.index_buffer.capacity < required_buffer_size {
+                // Resize index buffer, if needed.
+                self.index_buffer.capacity = (self.index_buffer.capacity * 2)
+                    .min(required_buffer_size);
+                self.index_buffer.inner = graphics.device().create_buffer(&wgpu::BufferDescriptor {
+                    label: Some("Painter Index Buffer"),
+                    usage: wgpu::BufferUsages::INDEX | wgpu::BufferUsages::COPY_DST,
+                    size: self.index_buffer.capacity,
+                    mapped_at_creation: false,
+                });
+            }
+
+            let index_buffer_staging = graphics.queue().write_buffer_with(
+                &self.index_buffer.inner,
+                0,
+                std::num::NonZeroU64::new(required_buffer_size).unwrap(),
+            );
+
+            let Some(mut index_buffer_staging) = index_buffer_staging else {
+                panic!(
+                    "Failed to create staging buffer for index data. Index count: {num_indices}. Required index buffer size: {required_buffer_size}. Actual size {} and capacity: {} (bytes)",
+                    self.index_buffer.inner.size(),
+                    self.index_buffer.capacity,
+                );
+            };
+
+            let mut index_offset = 0;
+            for paint in paints {
+                let size = paint.indices.len() * std::mem::size_of::<u32>();
+                let slice = index_offset..(size + index_offset);
+                index_buffer_staging[slice.clone()]
+                    .copy_from_slice(bytemuck::cast_slice(&paint.indices));
+                self.index_buffer.slices.push(slice);
+                index_offset += size;
+            }
         }
-        pass.use_vertex_buffer(0, self.vertex_buffer.slice(..));
-        pass.use_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint32);
-        pass.draw_indexed(0..self.num_indices, 0, 0..1);
+
+        if num_vertices > 0 {
+            self.vertex_buffer.slices.clear();
+
+            let required_buffer_size = (std::mem::size_of::<Vertex>() * num_vertices) as u64;
+            if self.vertex_buffer.capacity < required_buffer_size {
+                // Resize vertex buffer if needed.
+                self.vertex_buffer.capacity = (self.vertex_buffer.capacity * 2)
+                    .min(required_buffer_size);
+                self.vertex_buffer.inner = graphics.device().create_buffer(&wgpu::BufferDescriptor {
+                    label: Some("Painter Vertex Buffer"),
+                    usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
+                    size: self.vertex_buffer.capacity,
+                    mapped_at_creation: false,
+                });
+            }
+
+            let vertex_buffer_staging = graphics.queue().write_buffer_with(
+                &self.vertex_buffer.inner,
+                0,
+                std::num::NonZeroU64::new(required_buffer_size).unwrap(),
+            );
+
+            let Some(mut vertex_buffer_staging) = vertex_buffer_staging else {
+                panic!(
+                    "Failed to create staging buffer for vertex data. Vertex count: {num_vertices}. Required vertex buffer size: {required_buffer_size}. Actual size {} and capacity: {} (bytes)",
+                    self.vertex_buffer.inner.size(),
+                    self.vertex_buffer.capacity,
+                );
+            };
+
+            let mut vertex_offset = 0;
+            for paint in paints {
+                let size = paint.vertices.len() * std::mem::size_of::<Vertex>();
+                let slice = vertex_offset..(size + vertex_offset);
+                vertex_buffer_staging[slice.clone()]
+                    .copy_from_slice(bytemuck::cast_slice(&paint.vertices));
+                self.vertex_buffer.slices.push(slice);
+                vertex_offset += size;
+            }
+        }
+    }
+
+    pub fn render(&self, mut pass: RenderPass, paints: &[PaintMesh]) {
+        pass.use_shader(&self.shader);
+
+        let mut index_buffer_slices = self.index_buffer.slices.iter();
+        let mut vertex_buffer_slices = self.vertex_buffer.slices.iter();
+
+        for paint in paints {
+            let index_buffer_slice = index_buffer_slices.next().unwrap();
+            let vertex_buffer_slice = vertex_buffer_slices.next().unwrap();
+
+            pass.use_index_buffer(
+                self.index_buffer.inner.slice(
+                    index_buffer_slice.start as u64..index_buffer_slice.end as u64,
+                ),
+                wgpu::IndexFormat::Uint32,
+            );
+            pass.use_vertex_buffer(
+                0,
+                self.vertex_buffer.inner.slice(
+                    vertex_buffer_slice.start as u64..vertex_buffer_slice.end as u64,
+                ),
+            );
+            pass.draw_indexed(0..paint.indices.len() as u32, 0, 0..1);
+        }
     }
 }
 
-pub enum Paint {
-    Quad {
-        pos: Vec2,
-        size: Vec2,
-    },
+pub struct PaintMesh {
+    indices: Vec<u32>,
+    vertices: Vec<Vertex>,
+}
+
+pub struct PaintBuffer {
+    inner: wgpu::Buffer,
+    slices: Vec<Range<usize>>,
+    capacity: wgpu::BufferAddress,
 }
