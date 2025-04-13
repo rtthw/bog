216401f4d7fa907cce84db305f7cae59b132e211
rtthw <172864013+rtthw@users.noreply.github.com>
2025-04-13 06:21:19 -0400

    Implement font face loading

--- src/fonts.rs
+++ src/fonts.rs
@@ -10,14 +10,29 @@ type Result<T> = std::result::Result<T, FontError>;
 
 #[derive(thiserror::Error, Debug)]
 pub enum FontError {
+    #[error("attempted to load invalid font")]
+    InvalidFont,
     #[error("face parsing error")]
     ParsingError(#[from] ttf_parser::FaceParsingError),
 }
 
 
 
-pub fn load_font(bytes: &[u8]) -> Result<FontFace> {
-    todo!()
+pub fn load_font_face(bytes: &[u8]) -> Result<FontFace> {
+    let font_count = ttf_parser::fonts_in_collection(bytes)
+        .ok_or(FontError::InvalidFont)?;
+
+    // TODO: Support font collections?
+    if font_count != 0 {
+        return Err(FontError::InvalidFont);
+    }
+
+    let face = FontFace {
+        data: Arc::new(bytes.to_vec()),
+        index: font_count,
+    };
+
+    Ok(face)
 }
 
 
@@ -44,7 +59,7 @@ pub struct ParsedFontFace<'a> {
 }
 
 impl<'a> ParsedFontFace<'a> {
-    pub fn glyph_is_colored(&self, id: u16) -> bool {
-        self.inner.is_color_glyph(ttf_parser::GlyphId(id))
+    pub fn line_gap(&self) -> i16 {
+        self.inner.line_gap()
     }
 }
