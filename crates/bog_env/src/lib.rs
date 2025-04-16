b5495fd3b0f063631f4b0e9f51c22f027c73b898
rtthw <172864013+rtthw@users.noreply.github.com>
2025-04-16 09:27:11 -0400

    Add some useful methods to the platform type

--- crates/bog_env/src/lib.rs
+++ crates/bog_env/src/lib.rs
@@ -2,16 +2,83 @@
 
 
 
-pub struct Platform {
-    pub cpu_arch: &'static str,
-    pub os_name: &'static str,
-    pub os_family: &'static str,
-}
-
 pub const fn current_platform() -> Platform {
     Platform {
         cpu_arch: std::env::consts::ARCH,
-        os_name: std::env::consts::OS,
         os_family: std::env::consts::FAMILY,
+        os_name: std::env::consts::OS,
+    }
+}
+
+pub struct Platform {
+    pub cpu_arch: &'static str,
+    pub os_family: &'static str,
+    pub os_name: &'static str,
+}
+
+impl Platform {
+    pub fn could_be_desktop(&self) -> bool {
+        match (self.os_family, self.os_name) {
+            (_, "linux") => true,
+            (_, "redox") => true,
+            (_, "windows") => true,
+            (_, "macos") => true,
+            (_, "openbsd") => true,
+            (_, "freebsd") => true,
+            (_, "netbsd") => true,
+            (_, "haiku") => true,
+
+            _ => false,
+        }
+    }
+
+    pub fn could_be_embedded(&self) -> bool {
+        match (self.os_family, self.os_name) {
+            (_, "linux") => true,
+            (_, "redox") => true,
+            (_, "openbsd") => true,
+            (_, "freebsd") => true,
+            (_, "netbsd") => true,
+            ("itron", _) => true,
+
+            _ => false,
+        }
+    }
+
+    pub fn could_be_mobile(&self) -> bool {
+        match (self.os_family, self.os_name) {
+            (_, "linux") => true,
+            (_, "redox") => true,
+            (_, "android") => true,
+            (_, "ios") => true,
+
+            _ => false,
+        }
+    }
+
+    pub fn could_be_web(&self) -> bool {
+        match (self.os_family, self.os_name) {
+            (_, "wasi") => true,
+
+            _ => false,
+        }
+    }
+
+    pub fn could_support_display(&self) -> bool {
+        match (self.os_family, self.os_name) {
+            (_, "linux") => true,
+            (_, "redox") => true,
+            (_, "windows") => true,
+            (_, "macos") => true,
+            (_, "android") => true,
+            (_, "ios") => true,
+            (_, "openbsd") => true,
+            (_, "freebsd") => true,
+            (_, "netbsd") => true,
+            (_, "haiku") => true,
+            (_, "wasi") => true,
+
+            _ => false,
+        }
     }
 }
