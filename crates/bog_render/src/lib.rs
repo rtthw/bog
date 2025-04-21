a0cd3fbecd59de6b4745e491299b5a69206e01ea
rtthw <172864013+rtthw@users.noreply.github.com>
2025-04-21 10:06:02 -0400

    Setup new quad rendering pipeline

--- crates/bog_render/src/lib.rs
+++ crates/bog_render/src/lib.rs
@@ -3,3 +3,170 @@
 
 
 pub mod primitive;
+
+
+
+pub struct Renderer {
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    format: wgpu::TextureFormat,
+
+    quad_pipeline: QuadPipeline,
+}
+
+impl Renderer {
+    pub fn new(device: wgpu::Device, queue: wgpu::Queue, format: wgpu::TextureFormat) -> Self {
+        let quad_pipeline = QuadPipeline::new(&device, format);
+
+        Self {
+            device,
+            queue,
+            format,
+
+            quad_pipeline,
+        }
+    }
+}
+
+
+
+#[derive(Clone, Copy, Debug)]
+#[derive(bytemuck::Pod, bytemuck::Zeroable)]
+#[repr(C)]
+struct Uniforms {
+    transform: [f32; 16],
+    scale: f32,
+    _padding: [f32; 3], // Align to `mat4x4<f32>`.
+}
+
+#[derive(Clone, Copy, Debug)]
+#[derive(bytemuck::Pod, bytemuck::Zeroable)]
+#[repr(C)]
+pub struct Quad {
+    pub position: [f32; 2],
+    pub size: [f32; 2],
+    pub border_color: [f32; 4], // linear rgb
+    pub border_radius: [f32; 4], // pqdb ordering
+    pub border_width: f32,
+    pub shadow_color: [f32; 4], // linear rgb
+    pub shadow_offset: [f32; 2],
+    pub shadow_blur_radius: f32,
+}
+
+#[derive(Clone, Copy, Debug)]
+#[derive(bytemuck::Pod, bytemuck::Zeroable)]
+#[repr(C)]
+pub struct QuadSolid {
+    pub color: [f32; 4], // linear rgb
+    pub quad: Quad,
+}
+
+struct QuadPipeline {
+    pipeline: wgpu::RenderPipeline,
+}
+
+impl QuadPipeline {
+    fn new(device: &wgpu::Device, format: wgpu::TextureFormat) -> Self {
+        let constants_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: Some("bog::uniforms_layout::quad"),
+            entries: &[wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStages::VERTEX,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Uniform,
+                    has_dynamic_offset: false,
+                    min_binding_size: wgpu::BufferSize::new(
+                        std::mem::size_of::<Uniforms>() as wgpu::BufferAddress,
+                    ),
+                },
+                count: None,
+            }],
+        });
+
+        let layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: Some("bog::pipeline_layout::quad"),
+            push_constant_ranges: &[],
+            bind_group_layouts: &[&constants_layout],
+        });
+
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some("bog::shader::quad"),
+            source: wgpu::ShaderSource::Wgsl(std::borrow::Cow::Borrowed(
+                include_str!("shaders/quad.wgsl"),
+            )),
+        });
+
+        let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("bog::pipeline::quad"),
+            layout: Some(&layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: Some("vs_main"),
+                buffers: &[wgpu::VertexBufferLayout {
+                    array_stride: std::mem::size_of::<QuadSolid>() as u64,
+                    step_mode: wgpu::VertexStepMode::Instance,
+                    attributes: &wgpu::vertex_attr_array!(
+                        // Color.
+                        0 => Float32x4,
+                        // Position.
+                        1 => Float32x2,
+                        // Size.
+                        2 => Float32x2,
+                        // Border color.
+                        3 => Float32x4,
+                        // Border radius.
+                        4 => Float32x4,
+                        // Border width.
+                        5 => Float32,
+                        // Shadow color.
+                        6 => Float32x4,
+                        // Shadow offset.
+                        7 => Float32x2,
+                        // Shadow blur radius.
+                        8 => Float32,
+                    ),
+                }],
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: Some("fs_main"),
+                targets: &[Some(wgpu::ColorTargetState {
+                    format,
+                    blend: Some(wgpu::BlendState {
+                        color: wgpu::BlendComponent {
+                            src_factor: wgpu::BlendFactor::SrcAlpha,
+                            dst_factor: wgpu::BlendFactor::OneMinusSrcAlpha,
+                            operation: wgpu::BlendOperation::Add,
+                        },
+                        alpha: wgpu::BlendComponent {
+                            src_factor: wgpu::BlendFactor::One,
+                            dst_factor: wgpu::BlendFactor::OneMinusSrcAlpha,
+                            operation: wgpu::BlendOperation::Add,
+                        },
+                    }),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+                compilation_options:
+                    wgpu::PipelineCompilationOptions::default(),
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                front_face: wgpu::FrontFace::Cw,
+                ..Default::default()
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+            cache: None,
+        });
+
+        Self {
+            pipeline,
+        }
+    }
+}
