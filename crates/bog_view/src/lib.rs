82f2944d186065551fd6e7a2dce95832520e6de5
rtthw <172864013+rtthw@users.noreply.github.com>
2025-05-14 07:44:54 -0400

    Write out most of the object rendering functionality

--- crates/bog_view/src/lib.rs
+++ crates/bog_view/src/lib.rs
@@ -3,7 +3,9 @@
 
 
 use bog_collections::NoHashMap;
-use bog_layout::Layout;
+use bog_layout::{Layout, Placement};
+use bog_math::Rect;
+use bog_render::{Render as _, Renderer};
 
 
 
@@ -13,15 +15,16 @@ pub trait View {
 
 
 
-/// A view model really just a tree of [`Element`]s that have been
+/// A view model really just a tree of [`Element`]s that have been attached to the [`View`].
 pub struct Model<V: View> {
-    elements: NoHashMap<u64, ElementProxy<V>>,
+    elements: NoHashMap<u64, Option<Box<dyn Object<View = V>>>>,
     root_node: u64,
 }
 
-/// An element proxy is useful because we need
-struct ElementProxy<V: View> {
-    object: Option<Box<dyn Object<View = V>>>,
+impl<V: View> Model<V> {
+    pub fn grab(&mut self, node: u64) -> Option<Box<dyn Object<View = V>>> {
+        self.elements.insert(node, None).and_then(|mut o| o.take())
+    }
 }
 
 
@@ -40,11 +43,74 @@ pub struct Element<V: View> {
 pub trait Object {
     type View: View;
 
-    fn render(&mut self, view: &mut Self::View, cx: Context<Self::View>) {}
+    fn render(&mut self, render: Render<Self::View>) {}
+    fn pre_render(&mut self, render: Render<Self::View>) {}
+    fn post_render(&mut self, render: Render<Self::View>) {}
 }
 
 
 
 pub struct Context<'a, V: View> {
+    pub view: &'a mut V,
     pub model: &'a mut Model<V>,
+}
+
+
+
+pub struct Render<'a, V: View> {
+    pub cx: Context<'a, V>,
+    pub renderer: &'a mut Renderer,
+}
+
+pub fn render_view<V: View>(
+    model: &mut Model<V>,
+    view: &mut V,
+    renderer: &mut Renderer,
+    root_placement: Placement,
+    viewport_rect: Rect,
+) {
+    renderer.clear();
+    renderer.start_layer(viewport_rect);
+    render_placement(root_placement, model, view, renderer);
+    renderer.end_layer();
+}
+
+fn render_placement<V: View>(
+    placement: Placement,
+    model: &mut Model<V>,
+    view: &mut V,
+    renderer: &mut Renderer,
+) {
+    for child_placement in placement.children() {
+        if let Some(mut obj) = model.grab(child_placement.node()) {
+            obj.pre_render(Render {
+                cx: Context {
+                    model,
+                    view,
+                },
+                renderer,
+            });
+            obj.render(Render {
+                cx: Context {
+                    model,
+                    view,
+                },
+                renderer,
+            });
+            model.elements.insert(child_placement.node(), Some(obj));
+        }
+
+        render_placement(child_placement, model, view, renderer);
+
+        if let Some(mut obj) = model.grab(child_placement.node()) {
+            obj.post_render(Render {
+                cx: Context {
+                    model,
+                    view,
+                },
+                renderer,
+            });
+            model.elements.insert(child_placement.node(), Some(obj));
+        }
+    }
 }
