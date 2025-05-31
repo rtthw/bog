fed99eeb6e8c98ef03f4d152a1606631ae745f86
rtthw <172864013+rtthw@users.noreply.github.com>
2025-05-31 06:56:21 -0400

    Write out most of the element styling properties

--- crates/bog_view/src/style.rs
+++ crates/bog_view/src/style.rs
@@ -7,7 +7,7 @@ use bog_window::CursorIcon;
 
 
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default)]
 pub struct ElementStyle {
     pub cursor: Option<CursorIcon>,
     pub display: Option<DisplayStyle>,
@@ -23,6 +23,7 @@ pub struct ElementStyle {
     pub scale: Option<Unit>,
     pub gap_x: Option<Unit>,
     pub gap_y: Option<Unit>,
+
     pub inset_left: Option<Unit>,
     pub inset_right: Option<Unit>,
     pub inset_top: Option<Unit>,
@@ -31,12 +32,29 @@ pub struct ElementStyle {
     pub margin_right: Option<Unit>,
     pub margin_top: Option<Unit>,
     pub margin_bottom: Option<Unit>,
+    pub padding_left: Option<Unit>,
+    pub padding_right: Option<Unit>,
+    pub padding_top: Option<Unit>,
+    pub padding_bottom: Option<Unit>,
+    pub border_left: Option<Unit>,
+    pub border_right: Option<Unit>,
+    pub border_top: Option<Unit>,
+    pub border_bottom: Option<Unit>,
 
     pub font_size: Option<FontSize>,
     pub font_style: Option<TextSlant>,
     pub font_weight: Option<LineWeight>,
+    pub text_align: Option<TextAlign>,
 
+    pub color: Option<Color>,
     pub background_color: Option<Color>,
+
+    pub align_content: Option<PositioningStyle>,
+    pub align_items: Option<AlignStyle>,
+    pub align_self: Option<AlignStyle>,
+    pub justify_content: Option<PositioningStyle>,
+    pub justify_items: Option<AlignStyle>,
+    pub justify_self: Option<AlignStyle>,
 }
 
 
@@ -50,6 +68,30 @@ pub enum DisplayStyle {
     None,
 }
 
+#[derive(Clone, Copy, Debug)]
+pub enum AlignStyle {
+    Start,
+    End,
+    FlexStart,
+    FlexEnd,
+    Center,
+    Baseline,
+    Stretch,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum PositioningStyle {
+    Start,
+    End,
+    FlexStart,
+    FlexEnd,
+    Center,
+    Stretch,
+    SpaceBetween,
+    SpaceEvenly,
+    SpaceAround,
+}
+
 #[derive(Clone, Copy, Debug, Default)]
 pub enum FontSize {
     Small,
@@ -58,4 +100,13 @@ pub enum FontSize {
     Large,
     Length(Unit),
     Percent(f32),
+}
+
+#[derive(Clone, Copy, Debug, Default)]
+pub enum TextAlign {
+    #[default]
+    Left,
+    Right,
+    Center,
+    Justify,
 }
