ca3e82b4c3c3eecc322d01efd785860132b75c40
rtthw <172864013+rtthw@users.noreply.github.com>
2025-05-31 07:54:49 -0400

    Create length type

--- crates/bog_types/src/lib.rs
+++ crates/bog_types/src/lib.rs
@@ -1,1 +1,29 @@
 //! Bog types
+
+#![no_std]
+
+
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Length {
+    Px(f32),
+    Em(f32),
+    Lh(f32),
+    Rem(f32),
+    Rlh(f32),
+}
+
+#[inline]
+pub const fn px(value: f32) -> Length {
+    Length::Px(value)
+}
+
+#[inline]
+pub const fn em(value: f32) -> Length {
+    Length::Em(value)
+}
+
+#[inline]
+pub const fn lh(value: f32) -> Length {
+    Length::Lh(value)
+}
