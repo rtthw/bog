466e486af0fb0eb7b73a667e73843f5e8afa75f3
rtthw <172864013+rtthw@users.noreply.github.com>
2025-06-09 09:11:22 -0400

    Add diffing to view elements

--- crates/bog-view/src/lib.rs
+++ crates/bog-view/src/lib.rs
@@ -6,23 +6,47 @@ pub mod style;
 pub mod tree;
 
 
-use core::{any::TypeId, marker::PhantomData};
+use core::{any::TypeId, borrow::Borrow};
 
-use bog_core::{InputEvent, NoHashMap};
+use bog_core::InputEvent;
 use bog_render::RenderPass;
 use tree::ViewTree;
 
 
 
-pub trait View: 'static {
-    fn build(&self) -> Node;
+pub trait Element: 'static {
+    #[allow(unused_variables)]
+    fn render(&self, render: RenderContext) {}
+
+    #[allow(unused_variables)]
+    fn input(&mut self, input: InputContext) {}
+
+    fn tag(&self) -> Tag {
+        Tag::null()
+    }
+
+    fn diff(&self, node: &mut Node) {
+        node.children.clear();
+    }
+
+    fn children(&self) -> Vec<Node> {
+        Vec::new()
+    }
 }
 
-pub trait Element: 'static {
-    type View: View;
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct Tag(TypeId);
 
-    fn render(&self, render: RenderContext) {}
-    fn input(&mut self, input: InputContext) {}
+impl Default for Tag {
+    fn default() -> Self {
+        Self::null()
+    }
+}
+
+impl Tag {
+    pub fn null() -> Self {
+        Self(TypeId::of::<()>())
+    }
 }
 
 pub struct RenderContext<'a> {
@@ -37,105 +61,62 @@ pub struct InputContext<'a> {
 
 
 pub fn render_view(
-    view: &mut impl View,
     tree: &mut ViewTree,
-    root_element: &mut impl Element,
-    root_id: u64,
-    elements: NoHashMap<u64, Box<dyn ElementProxy>>,
+    root: &mut impl Element,
 ) {
-    view.build().0.crawl(root_id, tree, &mut |id, tree| {
-        todo!()
-    });
 }
 
 
 
-#[derive(PartialEq)]
-pub struct Node(InnerNode);
-
-// TODO: Better debug repr.
-impl core::fmt::Debug for Node {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Node::{}", match &self.0 {
-            InnerNode::None => "None",
-            InnerNode::Branch(_nodes) => "Branch",
-            InnerNode::Leaf(_node) => "Leaf",
-        })
-    }
+pub struct Node {
+    pub tag: Tag,
+    pub children: Vec<Node>,
 }
 
 impl Node {
-    pub fn leaf<E: Element>() -> Self {
-        Self(InnerNode::Leaf(Box::new(ElementNodeDef::<E>::new())))
-    }
+    pub fn new<'a, T: Borrow<dyn Element> + 'a>(element: T) -> Self {
+        let element = element.borrow();
 
-    pub fn branch(children: impl IntoIterator<Item = Node>) -> Self {
-        Self(InnerNode::Branch(children.into_iter().map(|n| n.0).collect()))
-    }
-
-    pub const fn none() -> Self {
-        Self(InnerNode::None)
-    }
-}
-
-#[derive(PartialEq)]
-enum InnerNode {
-    Leaf(Box<dyn ElementNode>),
-    Branch(Vec<InnerNode>),
-    None,
-}
-
-impl InnerNode {
-    /// ## Panics
-    ///
-    /// If the `tree` is not in sync with this node (i.e. this node does not have the same number
-    /// of children as the `tree` believes it does in [`ViewTree::children`]).
-    fn crawl(
-        &self,
-        id: u64,
-        tree: &mut ViewTree,
-        func: &mut impl FnMut(u64, &mut ViewTree),
-    ) {
-        match self {
-            InnerNode::Branch(nodes) => {
-                let ids = tree.children(id).to_owned();
-
-                assert!(nodes.len() == ids.len());
-
-                for (node, id) in nodes.iter().zip(ids) {
-                    node.crawl(id, tree, func);
-                }
-            }
-            InnerNode::Leaf(_node) => {
-                func(id, tree)
-            }
-            InnerNode::None => {}
+        Self {
+            tag: element.tag(),
+            children: element.children(),
         }
     }
-}
 
-trait ElementNode: 'static {
-    fn type_id(&self) -> TypeId {
-        TypeId::of::<Self>()
+    pub fn diff<'a, T: Borrow<dyn Element> + 'a>(&mut self, new: T) {
+        if self.tag == new.borrow().tag() {
+            new.borrow().diff(self);
+        } else {
+            *self = Self::new(new);
+        }
     }
-}
 
-impl PartialEq for Box<dyn ElementNode> {
-    fn eq(&self, other: &Self) -> bool {
-        self.type_id() == other.type_id()
+    pub fn diff_children<'a, T: Borrow<dyn Element> + 'a>(&mut self, new_children: &[T]) {
+        self.diff_children_custom(
+            new_children,
+            |node, element| node.diff(element.borrow()),
+            |element| Self::new(element.borrow()),
+        );
     }
-}
 
-struct ElementNodeDef<E: Element> {
-    _element: PhantomData<E>,
-}
+    pub fn diff_children_custom<T>(
+        &mut self,
+        new_children: &[T],
+        diff: impl Fn(&mut Node, &T),
+        new_state: impl Fn(&T) -> Self,
+    ) {
+        if self.children.len() > new_children.len() {
+            self.children.truncate(new_children.len());
+        }
 
-impl<E: Element> ElementNode for ElementNodeDef<E> {}
+        for (child_state, new) in self.children.iter_mut().zip(new_children.iter()) {
+            diff(child_state, new);
+        }
 
-impl<E: Element> ElementNodeDef<E> {
-    fn new() -> Self {
-        Self {
-            _element: PhantomData,
+        if self.children.len() < new_children.len() {
+            self.children.extend(
+                new_children[self.children.len()..].iter().map(new_state),
+            );
         }
     }
 }
@@ -150,39 +131,10 @@ pub trait ElementProxy {
 mod tests {
     use super::*;
 
-    struct TestView;
     struct TestElement;
     struct OtherElement;
 
-    impl View for TestView {
-        fn build(&self) -> Node {
-            Node::branch([
-                Node::leaf::<TestElement>(),
-                Node::branch([
-                    Node::leaf::<OtherElement>(),
-                    Node::leaf::<TestElement>(),
-                ]),
-                Node::leaf::<OtherElement>(),
-                Node::leaf::<OtherElement>(),
-            ])
-        }
-    }
+    impl Element for TestElement {}
 
-    impl Element for TestElement {
-        type View = TestView;
-    }
-
-    impl Element for OtherElement {
-        type View = TestView;
-    }
-
-    #[test]
-    fn node_equality() {
-        let a = Node::leaf::<TestElement>();
-        let b = Node::leaf::<OtherElement>();
-        let c = Node::leaf::<TestElement>();
-
-        assert_ne!(a, b);
-        assert_eq!(c, a);
-    }
+    impl Element for OtherElement {}
 }
